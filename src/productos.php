943e68f2dd26552922cd8013d4769364a11f1967
dancg <dannycalgon@gmail.com>
2018-03-14 09:54:18 +0100

    Creo la página de productos y borro la carpeta usuario

--- src/productos.php
+++ src/productos.php
@@ -14,17 +14,16 @@ $result = mysqli_query($mysqli, "SELECT * FROM producto JOIN fabricante ON (prod
 </head>
 
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
 	<table width='80%' border=0>
 
 	<tr bgcolor='#CCCCCC'>
-		<td>Código</td>
-		<td>Nombre</td>
+		<td>Código Producto</td>
+		<td>Nombre Fabriicante</td>
         <td>Código Fabricante</td>
         <td>Producto</td>
-        <td>Precio</td>
+        <td>Precio(€)</td>
 		<td>Imagen</td>
-		<td>descripcion</td>
+		<td>Descripcion</td>
 
                 
 	</tr>
@@ -39,7 +38,6 @@ $result = mysqli_query($mysqli, "SELECT * FROM producto JOIN fabricante ON (prod
 		echo "<td>".$res['precio']."</td>";
 		echo "<td><img src=\"".$res['imagen']."\" width=\"50\" height=\"50\" /</td>";
 		echo "<td>".$res['descripcion']."</td>";
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
 	}
 	mysqli_close($msqli);
 	?>
